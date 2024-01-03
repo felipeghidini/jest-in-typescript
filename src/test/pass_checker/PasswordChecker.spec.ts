68473e60970e9d542d35eeaf0b854ec1ea5e49c7
felipeghidini <lipeghidini18@gmail.com>
2024-01-03 17:26:03 -0300

    feat(password): password com numeros

--- src/test/pass_checker/PasswordChecker.spec.ts
+++ src/test/pass_checker/PasswordChecker.spec.ts
@@ -40,10 +40,21 @@ describe('PasswordChecker test suite', () => {
     expect(actual.valid).toBe(false);
     expect(actual.reasons).not.toContain(PasswordErrors.NO_LOWER_CASE);
   });
-
+  
   it('Complex password is valid', () => {
     const actual = sut.checkPassword('1234ABCDe');
     expect(actual.reasons).toHaveLength(0);
     expect(actual.valid).toBe(true);
+  });
+  
+  it('Admin password with no number is invalid', () => {
+    const actual = sut.checkAdminPassword('ABCDefghi');
+    expect(actual.reasons).toContain(PasswordErrors.NO_NUMBER);
+    expect(actual.valid).toBe(false);
+  });
+  
+  it('Admin password with number is valid', () => {
+    const actual = sut.checkAdminPassword('ABCDefghi1');
+    expect(actual.reasons).not.toContain(PasswordErrors.NO_NUMBER);
   });
 })
